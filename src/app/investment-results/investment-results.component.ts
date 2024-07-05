80ed556b36d5d901b598b98e6a30ee1bffbcda03
rutv1k46 <rutviksolanki01@gmail.com>
2024-07-05 12:58:52 -0400

    calculate investment servicce

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,6 @@
 import { CurrencyPipe } from "@angular/common";
 import { Component, Input, input } from "@angular/core";
+import { InvestmentService } from "../investment.service";
 
 @Component({
   selector: "app-investment-results",
@@ -9,14 +10,10 @@ import { Component, Input, input } from "@angular/core";
   styleUrl: "./investment-results.component.css",
 })
 export class InvestmentResultsComponent {
-  results = input<
-    {
-      year: number;
-      interest: number;
-      valueEndOfYear: number;
-      annualInvestment: number;
-      totalInterest: number;
-      totalAmountInvested: number;
-    }[]
-  >();
+
+  constructor(private investmentService: InvestmentService){}
+
+  get results(){
+    return this.investmentService.resultsData
+  }
 }
